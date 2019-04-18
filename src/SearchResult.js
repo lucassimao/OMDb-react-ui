3eb0c48a6e2149a347bb7fb5928d7d930c961f31
Lucas Simao <Lucas Simao>
2019-04-18 11:03:19 -0300

    implementing responsive layout with media queries for the SearchResult component

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Consumer } from "./SearchContext";
 import spinner from "./img/Spinner-1s-200px.gif";
+import "./css/searchresult.css";
 
 const handleImgError = evt => {
   evt.target.onerror = null;
@@ -11,35 +12,43 @@ const handleImgError = evt => {
 const SearchResult = props => {
   return (
     <Consumer>
-      {ctx => (
-        <ul className="movie-list">
-          {ctx.searching ? (
+      {ctx => {
+        return ctx.searching ? (
+          <div className="loader">
             <img src={spinner} alt="wait ..." />
-          ) : ctx.results ? (
-            ctx.results.map((movie, index) => (
-              <li key={`${movie.imdbID}-${index}`}>
-                <h2 className="movie-title"> {movie.Title} </h2>
-                <img src={movie.Poster} onError={handleImgError} />
-                <section className="movie-details">
-                  <p className="movie-plot">{movie.Plot}</p>
-                  <span className="movie-year">Year: {movie.Year}</span>
-                  <span className="movie-type">Type: {movie.Type}</span>
-                  <p>
-                    <a
-                      target="_blank"
-                      href={`https://www.imdb.com/title/${movie.imdbID}`}
-                    >
-                      Link IMDb
-                    </a>
-                  </p>
-                </section>
-              </li>
-            ))
-          ) : (
-            <li>No results found</li>
-          )}
-        </ul>
-      )}
+          </div>
+        ) : (
+          <ul className="movie-list">
+            {ctx.results ? (
+              ctx.results.map((movie, index) => (
+                <li key={`${movie.imdbID}-${index}`}>
+                  <h2 className="movie-title">{movie.Title}</h2>
+                  <img
+                    className="movie-banner"
+                    src={movie.Poster}
+                    onError={handleImgError}
+                  />
+                  <section className="movie-details">
+                    <p className="movie-plot">{movie.Plot}</p>
+                    <span className="movie-year">Year: {movie.Year}</span>
+                    <span className="movie-type">Type: {movie.Type}</span>
+                    <p className="link-imdb">
+                      <a
+                        target="_blank"
+                        href={`https://www.imdb.com/title/${movie.imdbID}`}
+                      >
+                        Link IMDb
+                      </a>
+                    </p>
+                  </section>
+                </li>
+              ))
+            ) : (
+              <li>No results found</li>
+            )}
+          </ul>
+        );
+      }}
     </Consumer>
   );
 };
